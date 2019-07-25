2b6c5c6cca3e7c1163c70ae01149d0fae5183d91
yjr2268152113 <2268152113@qq.com>
2019-07-25 23:56:13 +0800

    sort

--- vue-program-hema/src/pages/Sort.vue
+++ vue-program-hema/src/pages/Sort.vue
@@ -8,6 +8,11 @@
         </div>
       </div>
     </lifeTop>
+    <div class="top"></div>
+    <div class="content" v-for="(item,index) in items" :key="index">
+      <img :src="item.src" alt=""/>
+      <div class="text">{{item.title}}</div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +22,65 @@ import Top from '@/components/Top'
 export default {
   data () {
     return {
-      id:2
+      id:2,
+      items:[
+        {
+          title:'时令水果',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'新鲜蔬菜',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'海鲜水产',
+          src:'/static/images/shuiguo.jpg',
+        }, {
+          title:'肉禽蛋品',
+          src:'/static/images/shuiguo.jpg',
+        }, {
+          title:'面点主食',
+          src:'/static/images/shuiguo.jpg',
+        },
+        {
+          title:'乳品烘培',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'餐饮小吃',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'熟食净菜',
+          src:'/static/images/shuiguo.jpg',
+        }, 
+        {
+          title:'粮油干货',
+          src:'/static/images/shuiguo.jpg',
+        },
+        {
+          title:'休闲零食',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'中外名酒',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'水饮冲调',
+          src:'/static/images/shuiguo.jpg',
+        }, {
+          title:'美妆个护',
+          src:'/static/images/shuiguo.jpg',
+        },
+         {
+          title:'母婴保健',
+          src:'/static/images/shuiguo.jpg',
+        }, {
+          title:'厨卫百货',
+          src:'/static/images/shuiguo.jpg',
+        }
+      ]
     }
   },
   components:{
@@ -40,7 +103,21 @@ export default {
 
 <style lang="stylus" scoped>
 .sort
+  .top  
+    height 120px
   .sortTop
+  .content
+    display inline-block
+    width 250px
+    height 222px
+    margin  36.8px 0px 36.8px 0px
+    img
+      margin 0px 10px 0px 40px
+      height 118px
+     
+    .text 
+        text-align center
+        
     .name
       color #ffffff
       text-align center
